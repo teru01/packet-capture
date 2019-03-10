96e1e55b8f88409dc05a3c62303a82d057004919
Teruya Ono <teruya@gmail.com>
2019-03-10 18:42:58 +0900

    udp and ipv6(fail)

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,11 @@ use pnet::datalink::Channel::Ethernet;
 use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
 use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::tcp::TcpPacket;
+use pnet::packet::udp::UdpPacket;
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::Packet;
 
 use std::env;
-
-fn handle_packet(ethernet: &EthernetPacket) {
-    match ethernet.get_ethertype() {
-        EtherTypes::Ipv4 => {
-            let header = Ipv4Packet::new(ethernet.payload());
-            if let Some(header) = header {
-                match header.get_next_level_protocol() {
-                    IpNextHeaderProtocols::Tcp => {
-                        let tcp = TcpPacket::new(header.payload());
-                        if let Some(tcp) = tcp {
-                            println!("Got a tcp packet {}:{} to {}:{}",
-                                header.get_source(),
-                                tcp.get_source(),
-                                header.get_destination(),
-                                tcp.get_destination()
-                            );
-                        }
-                    },
-                    _ => println!("ignoring non TCP packet"),
-                }
-            }
-        },
-        _ => println!("ignoring non ipv4 packet"),
-    }
-}
 
 fn main() {
     let interface_name = env::args().nth(1).unwrap();
@@ -55,13 +31,90 @@ fn main() {
 
     loop {
         match rx.next() {
-            Ok(packet) => {
-                let packet = EthernetPacket::new(packet).unwrap();
-                handle_packet(&packet);
+            Ok(frame) => {
+                let frame = EthernetPacket::new(frame).unwrap();
+                match ethernet.get_ethertype() {
+                    EtherTypes::Ipv4 => {
+                        ipv4_handler(&frame);
+                    },
+                    EtherTypes::Ipv6 => {
+                        ipv6_handler(&frame);
+                    }
+                    _ => {
+                        println!("not ipv4");
+                    }
+                }
             },
             Err(e) => {
                 panic!("failed to read: {}", e);
             }
         }
+    }
+}
+
+// fn print_data<T: Packet, U: Packet>(l3: T, l4: U, proto: &str) {
+//     println!("Received {} packet {}:{} to {}:{}",
+//         proto,
+//         l3.get_source(),
+//         l4.get_source(),
+//         l3.get_destination(),
+//         l4.get_destination()
+//     );
+// }
+
+fn ipv4_handler(ethernet: &EthernetPacket) {
+    if let Some(packet) = Ipv4Packet::new(ethernet.payload()){
+        match packet.get_next_level_protocol() {
+            IpNextHeaderProtocols::Tcp => {
+                tcp_handler(&packet);
+            },
+            IpNextHeaderProtocols::Udp => {
+                udp_handler(&packet);
+            },
+            _ => {
+                println!("not tcp or udp packet");
+            }
+        }
+    }
+}
+
+
+fn ipv6_handler(ethernet: &EthernetPacket) {
+    if let Some(packet) = Ipv6Packet::new(ethernet.payload()){
+        match packet.get_next_header() {
+            IpNextHeaderProtocols::Tcp => {
+                tcp_handler(&packet);
+            },
+            IpNextHeaderProtocols::Udp => {
+                udp_handler(&packet);
+            },
+            _ => {
+                println!("not tcp or udp packet");
+            }
+        }
+    }
+}
+
+fn tcp_handler(packet: &Packet) {
+    let tcp = TcpPacket::new(packet.payload());
+    if let Some(tcp) = tcp {
+        println!("Received TCP packet {}:{} to {}:{}",
+            packet.get_source(),
+            tcp.get_source(),
+            packet.get_destination(),
+            tcp.get_destination()
+        );
+    }
+}
+
+fn udp_handler(packet: &Packet) {
+    let udp = UdpPacket::new(packet.payload());
+    if let Some(udp) = udp {
+        println!("Received UDP packet {}:{} to {}:{}",
+            packet.get_source(),
+            udp.get_source(),
+            packet.get_destination(),
+            udp.get_destination()
+        );
     }
 }
