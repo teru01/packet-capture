2b1823a950998a77a88a47f93440b6ac0e6a1552
Teruya Ono <teruya@gmail.com>
2019-03-11 19:46:34 +0900

    pcap ipv6

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use pnet::datalink::{self, NetworkInterface};
 use pnet::datalink::Channel::Ethernet;
 use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
 use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::ipv6::Ipv6Packet;
 use pnet::packet::tcp::TcpPacket;
 use pnet::packet::udp::UdpPacket;
 use pnet::packet::ip::IpNextHeaderProtocols;
@@ -33,7 +34,7 @@ fn main() {
         match rx.next() {
             Ok(frame) => {
                 let frame = EthernetPacket::new(frame).unwrap();
-                match ethernet.get_ethertype() {
+                match frame.get_ethertype() {
                     EtherTypes::Ipv4 => {
                         ipv4_handler(&frame);
                     },
@@ -41,7 +42,7 @@ fn main() {
                         ipv6_handler(&frame);
                     }
                     _ => {
-                        println!("not ipv4");
+                        println!("not ipv4 or ipv6");
                     }
                 }
             },
@@ -52,15 +53,78 @@ fn main() {
     }
 }
 
-// fn print_data<T: Packet, U: Packet>(l3: T, l4: U, proto: &str) {
-//     println!("Received {} packet {}:{} to {}:{}",
-//         proto,
-//         l3.get_source(),
-//         l4.get_source(),
-//         l3.get_destination(),
-//         l4.get_destination()
-//     );
-// }
+fn print_data(l3: &GettableEndPoints, l4: &GettableEndPoints, proto: &str) {
+    println!("Received {} packet {}:{} to {}:{}",
+        proto,
+        l3.get_source(),
+        l4.get_source(),
+        l3.get_destination(),
+        l4.get_destination()
+    );
+}
+
+trait GettableEndPoints {
+    fn get_source(&self) -> String;
+    fn get_destination(&self) -> String;
+    fn get_payload(&self) -> &[u8];
+}
+
+impl<'a> GettableEndPoints for Ipv4Packet<'a> {
+    fn get_source(&self) -> String {
+        self.get_source().to_string()
+    }
+
+    fn get_destination(&self) -> String {
+        self.get_destination().to_string()
+    }
+
+    fn get_payload(&self) -> &[u8] {
+        self.payload()
+    }
+}
+
+impl<'a> GettableEndPoints for Ipv6Packet<'a> {
+    fn get_source(&self) -> String {
+        self.get_source().to_string()
+    }
+
+    fn get_destination(&self) -> String {
+        self.get_destination().to_string()
+    }
+
+    fn get_payload(&self) -> &[u8] {
+        self.payload()
+    }
+}
+
+impl<'a> GettableEndPoints for TcpPacket<'a> {
+    fn get_source(&self) -> String {
+        self.get_source().to_string()
+    }
+
+    fn get_destination(&self) -> String {
+        self.get_destination().to_string()
+    }
+
+    fn get_payload(&self) -> &[u8] {
+        self.payload()
+    }
+}
+
+impl<'a> GettableEndPoints for UdpPacket<'a> {
+    fn get_source(&self) -> String {
+        self.get_source().to_string()
+    }
+
+    fn get_destination(&self) -> String {
+        self.get_destination().to_string()
+    }
+
+    fn get_payload(&self) -> &[u8] {
+        self.payload()
+    }
+}
+
 
 fn ipv4_handler(ethernet: &EthernetPacket) {
     if let Some(packet) = Ipv4Packet::new(ethernet.payload()){
@@ -95,26 +159,16 @@ fn ipv6_handler(ethernet: &EthernetPacket) {
     }
 }
 
-fn tcp_handler(packet: &Packet) {
-    let tcp = TcpPacket::new(packet.payload());
+fn tcp_handler(packet: &GettableEndPoints) {
+    let tcp = TcpPacket::new(packet.get_payload());
     if let Some(tcp) = tcp {
-        println!("Received TCP packet {}:{} to {}:{}",
-            packet.get_source(),
-            tcp.get_source(),
-            packet.get_destination(),
-            tcp.get_destination()
-        );
+        print_data(packet, &tcp, "TCP");
     }
 }
 
-fn udp_handler(packet: &Packet) {
-    let udp = UdpPacket::new(packet.payload());
+fn udp_handler(packet: &GettableEndPoints) {
+    let udp = UdpPacket::new(packet.get_payload());
     if let Some(udp) = udp {
-        println!("Received UDP packet {}:{} to {}:{}",
-            packet.get_source(),
-            udp.get_source(),
-            packet.get_destination(),
-            udp.get_destination()
-        );
+        print_data(packet, &udp, "UDP");
     }
 }
